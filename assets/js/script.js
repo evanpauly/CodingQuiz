9711a31200fa71813928582bc70bcb11208f6519
Evan <evanpauly@hotmail.com>
2022-01-21 08:24:34 -0600

    started on js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+start = document.getElementById("start");
+startButton = document.getElementById("startButton");
+questions = document.getElementById("questions");
+question1 =document.getElementById("question1");
+question2 =document.getElementById("question2");
+question3 =document.getElementById("question3");
+question4 =document.getElementById("question4");
