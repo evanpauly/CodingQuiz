56019f6eb867c79ae7f0276b1ce219c5e02f74b2
Evan <evanpauly@hotmail.com>
2022-01-21 12:56:53 -0600

    figuring out the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,64 @@
 start = document.getElementById("start");
 startButton = document.getElementById("startButton");
 questions = document.getElementById("questions");
-question1 =document.getElementById("question1");
-question2 =document.getElementById("question2");
-question3 =document.getElementById("question3");
-question4 =document.getElementById("question4");
+response1 = document.getElementById("response1");
+response2 = document.getElementById("response2");
+response3 = document.getElementById("response3");
+response4 = document.getElementById("response4");
+highScores = document.getElementById("high-scores");
+initials = document.getElementById("initials");
+
+var quizQuestions = [
+    {
+        question: "",
+        responses: {
+            response1: '',
+            response2: '',
+            response3: '',
+            response4: ''
+        },
+        correctAnswer: 'response1'
+    },
+    {
+        question: "",
+        responses: {
+            response1: '',
+            response2: '',
+            response3: '',
+            response4: ''
+        },
+        correctAnswer: 'response3'
+    },
+    {
+        question: "",
+        responses: {
+            response1: '',
+            response2: '',
+            response3: '',
+            response4: ''
+        },
+        correctAnswer: 'response4'
+    },
+    {
+        question: "",
+        responses: {
+            response1: '',
+            response2: '',
+            response3: '',
+            response4: ''
+        },
+        correctAnswer: 'response2'
+    }
+];
+
+function generateQuestions(questions){
+    var output = [];
+    var responses;
+
+    for(var i=0; i<questions.length; i++){
+
+        responses = [];
+
+        
+    }
+}
